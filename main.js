7dadb5c3175390dced327b0c69cb237e27d88866
Jazz Rudolph <jazzrudolph92@outlook.com>
2022-08-19 04:19:09 -0400

    The styling of the website is not complete. I am working on my javascript code and why its not working when you press the button, once that is resolved I will complete the styling.

---
+++ main.js
@@ -0,0 +1,18 @@
+let response = [
+    'NOPE',
+    'Its the NO for me!',
+    'Maybe Just Maybe',
+    '100% YES',
+    'Life is too short to be saying no.',
+    'Lets say I agreed to this. But, can I still change my mind later?',
+    'Ask me again in a few years.',
+    'Absolutely'
+]
+
+let displayAnswer = function () {
+    let index = Math.floor(Math.random() * answers.length);
+    let answer = answers[index];
+    let element = document.getElementById("circle");
+    element.style.display = "inline-block";
+    element.innerHTML = "<br><br>" + answer;
+  };
