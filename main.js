efe9e3573523f1e862bd831206b572f6498afbec
Jazz Rudolph <jazzrudolph92@outlook.com>
2022-08-20 02:16:33 -0400

    I completed the style, validated my code, and fixed all error I was having. Next I will do the Readme.md and the second user story.

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ let answers = [
     'Maybe Just Maybe',
     '100% YES',
     'Life is too short to be saying no.',
-    'Lets say I agreed to this. But, can I still change my mind later?',
+    'Lets say I agreed to this. But, can I change my mind later?',
     'Ask me again in a few years.',
     'Absolutely'
 ]
@@ -13,6 +13,7 @@ let displayAnswer = function () {
     let index = Math.floor(Math.random() * answers.length);
     let answer = answers[index];
     let element = document.getElementById("circle");
-    element.style.display = "inline-block";
+    element.style.display = "flex";
+    element.style.justifyContent = "center";
     element.innerHTML = "<br><br>" + answer;
   };
